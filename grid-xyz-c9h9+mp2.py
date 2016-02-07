3ea830579d60b0d1c192798b4827799f4323e91c
elekezem <hasalready@gmail.com>
2016-02-07 07:54:32 +0000

    change range function to increase usefulness

--- grid-xyz-c9h9+mp2.py
+++ grid-xyz-c9h9+mp2.py
@@ -1,10 +1,7 @@
 #
-# Reflects the partial 3d grid for C2 c9h9+ as
-# (x,y,z) -> (-x,-y,z)
-# and prints GAUSSIAN cube data in x, y, z order
-# (z changes fastest, followed by y and x)
+# Author:MAKE_DI Institute:University of York
+
 import csv
-import operator
 
 
 ix = []
@@ -12,31 +9,33 @@ iy = []
 iz = []
 siso = []
 br = 0.5291772083
-nx = 70
-ny = 90
-nz = 70
-delta = 0.05
 
-def grid_gen():
-    for x in range(-nx, nx + 1, 1):
-        x = x * delta
-        for y in range(-ny, ny + 1, 1):
-            y = y * delta
-            for z in range(-nz + 10, nz + 11, 1):
-                z = z * delta
-                ix.append(round(x, 6))
-                iy.append(round(y, 6))
-                iz.append(round(z, 6))
+
+def drange(start, stop, step):
+    R = []
+    r = start
+    while r < stop:
+        yield r
+        R.append(r)
+        r += step
+    return R
+
+
+def grid_gen(x_start, x_stop, y_start, y_stop, z_start, z_stop, delta):
+    for x in drange(x_start, x_stop, delta):
+        for y in drange(y_start, y_stop, delta):
+            for z in drange(z_start, z_stop, delta):
+                ix.append("%.6f" % round(x, 6))
+                iy.append("%.6f" % round(y, 6))
+                iz.append("%.6f" % round(z, 6))
     return ix, iy, iz
 
 
 ########################################################################################
-grid_gen()
-print(len(ix))
+grid_gen(-3.5, 3.5, 0.0, 4.5, -3.0, 4.0, 0.05)
+print('Total Bq atoms number is', len(ix))
 with open('cube_grid', 'w', newline='') as csvfile:
     cubewriter = csv.writer(csvfile, delimiter='\t',
                             quotechar=' ', quoting=csv.QUOTE_MINIMAL)
     for i in range(len(ix)):
-        cubewriter.writerow([ix[i],iy[i],iz[i]])
-    pass
-
+        cubewriter.writerow([ix[i], iy[i], iz[i]])
--- list.py
+++ list.py
@@ -1,11 +1,41 @@
-import os
+#
+# Author:MAKE_DI Institute:University of York
+
 import csv
 
-n = []
-for k in range(1,200):
-    n.append(k)
-with open('list.csv', 'w', newline='') as csvfile:
-    cubewriter = csv.writer(csvfile, delimiter=' ')
-    for c in range(len(n)):
-        cubewriter.writerow([n[c]])
-    pass
+
+ix = []
+iy = []
+iz = []
+siso = []
+br = 0.5291772083
+
+
+def drange(start, stop, step):
+    R = []
+    r = start
+    while r < stop:
+        yield r
+        R.append(r)
+        r += step
+    return R
+
+
+def grid_gen(x_start, x_stop, y_start, y_stop, z_start, z_stop, delta):
+    for x in drange(x_start, x_stop, delta):
+        for y in drange(y_start, y_stop, delta):
+            for z in drange(z_start, z_stop, delta):
+                ix.append("%.6f" % round(x, 6))
+                iy.append("%.6f" % round(y, 6))
+                iz.append("%.6f" % round(z, 6))
+    return ix, iy, iz
+
+
+########################################################################################
+grid_gen(-3.5, 3.5, 0.0, 4.5, -3.0, 4.0, 0.05)
+print('Total Bq atoms number is', len(ix))
+with open('cube_grid', 'w', newline='') as csvfile:
+    cubewriter = csv.writer(csvfile, delimiter='\t',
+                            quotechar=' ', quoting=csv.QUOTE_MINIMAL)
+    for i in range(len(ix)):
+        cubewriter.writerow([ix[i], iy[i], iz[i]])
