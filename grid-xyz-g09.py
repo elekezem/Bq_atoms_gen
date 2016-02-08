1aad16a1baccdbe889b70b0897aee3badc2c704a
elekezem <hasalready@gmail.com>
2016-02-08 14:12:25 +0000

    fix bq & add 2d , fix bugs

--- grid-xyz-g09.py
+++ grid-xyz-g09.py
@@ -74,13 +74,13 @@ def main():
             cube_writer = csv.writer(csv_file, delimiter='\t')
 
             # set cores number to calculate
-            cube_writer.writerow(['%NProc=4'])
+            cube_writer.writerow(['%NProc=2'])
 
             # set mem size to calculate
-            cube_writer.writerow(['%Mem=6Gb'])
+            cube_writer.writerow(['%Mem=2Gb'])
 
             # g09 key words
-            cube_writer.writerow(['# HF/6-311++G(d,p) SCF(Tight) NMR CPHF(Separate) Test '])
+            cube_writer.writerow(['# HF/6-311++G(d,p) SCF(Tight) NMR CPHF(Separate) Test'])
 
             # space line
             cube_writer.writerow([])
@@ -91,32 +91,35 @@ def main():
             # space line
             cube_writer.writerow([])
 
+            # eletron
+            cube_writer.writerow(['+1 1'])
+
             # Geo
-            cube_writer.writerow(['C 0 0. 0. 1.645014194'])
-            cube_writer.writerow(['C 0 0.4905022324 1.2427684773 1.1554414148'])
-            cube_writer.writerow(['C 0 -0.4905022324 -1.2427684773 1.1554414148'])
-            cube_writer.writerow(['C 0 0.0100585215 2.0928052569 0.1040785875'])
-            cube_writer.writerow(['C 0 -0.0100585215 -2.0928052569 0.1040785875'])
-            cube_writer.writerow(['C 0 -0.8152087457 1.6410894083 -0.9354914584'])
-            cube_writer.writerow(['C 0 0.8152087457 -1.6410894083 -0.9354914584'])
-            cube_writer.writerow(['C 0 -0.6569284614 0.2832859329 -1.251946014'])
-            cube_writer.writerow(['C 0 0.6569284614 -0.2832859329 -1.251946014'])
-            cube_writer.writerow(['H 0 0. 0. 2.7459418542'])
-            cube_writer.writerow(['H 0 1.0699783805 1.7858874614 1.9137457041'])
-            cube_writer.writerow(['H 0 -1.0699783805 -1.7858874614 1.9137457041'])
-            cube_writer.writerow(['H 0 0.12964425 3.1677255378 0.2859658405'])
-            cube_writer.writerow(['H 0 -0.12964425 -3.1677255378 0.2859658405'])
-            cube_writer.writerow(['H 0 -1.5936377795 2.2790433661 -1.3665504299'])
-            cube_writer.writerow(['H 0 1.5936377795 -2.2790433661 -1.3665504299'])
-            cube_writer.writerow(['H 0 -1.490812806 -0.3325910524 -1.6109751404'])
-            cube_writer.writerow(['H 0 1.490812806 0.3325910524 -1.6109751404'])
+            cube_writer.writerow(['C,0,0.,0.,1.645014194'])
+            cube_writer.writerow(['C,0,0.4905022324,1.2427684773,1.1554414148'])
+            cube_writer.writerow(['C,0,-0.4905022324,-1.2427684773,1.1554414148'])
+            cube_writer.writerow(['C,0,0.0100585215,2.0928052569,0.1040785875'])
+            cube_writer.writerow(['C,0,-0.0100585215,-2.0928052569,0.1040785875'])
+            cube_writer.writerow(['C,0,-0.8152087457,1.6410894083,-0.9354914584'])
+            cube_writer.writerow(['C,0,0.8152087457,-1.6410894083,-0.9354914584'])
+            cube_writer.writerow(['C,0,-0.6569284614,0.2832859329,-1.251946014'])
+            cube_writer.writerow(['C,0,0.6569284614,-0.2832859329,-1.251946014'])
+            cube_writer.writerow(['H,0,0.,0.,2.7459418542'])
+            cube_writer.writerow(['H,0,1.0699783805,1.7858874614,1.9137457041'])
+            cube_writer.writerow(['H,0,-1.0699783805,-1.7858874614,1.9137457041'])
+            cube_writer.writerow(['H,0,0.12964425,3.1677255378,0.2859658405'])
+            cube_writer.writerow(['H,0,-0.12964425,-3.1677255378,0.2859658405'])
+            cube_writer.writerow(['H,0,-1.5936377795,2.2790433661,-1.3665504299'])
+            cube_writer.writerow(['H,0,1.5936377795,-2.2790433661,-1.3665504299'])
+            cube_writer.writerow(['H,0,-1.490812806,-0.3325910524,-1.6109751404'])
+            cube_writer.writerow(['H,0,1.490812806,0.3325910524,-1.6109751404'])
 
             # space line
             cube_writer.writerow([])
 
             # set Bq atom coord in number of max_bq_num
             for j in range(len(chunks_by_bq[i])):
-                cube_writer.writerow(['Bq ', '0',
+                cube_writer.writerow(['Bq', '0',
                                       ix[max_bq_num*i+j],
                                       iy[max_bq_num*i+j],
                                       iz[max_bq_num*i+j]])
