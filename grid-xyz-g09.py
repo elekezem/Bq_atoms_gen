22dbda191d511852bbf74bfa072c28cf578029ed
elekezem <hasalready@gmail.com>
2016-02-08 12:11:41 +0000

    fix bq

--- grid-xyz-g09.py
+++ grid-xyz-g09.py
@@ -116,7 +116,7 @@ def main():
 
             # set Bq atom coord in number of max_bq_num
             for j in range(len(chunks_by_bq[i])):
-                cube_writer.writerow(['Bq ',
+                cube_writer.writerow(['Bq ', '0',
                                       ix[max_bq_num*i+j],
                                       iy[max_bq_num*i+j],
                                       iz[max_bq_num*i+j]])
