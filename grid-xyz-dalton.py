0f84788528cfba96a6d9f850fee286d2bb80a293
elekezem <hasalready@gmail.com>
2016-02-10 08:17:30 +0000

    add run.sh gen

--- grid-xyz-dalton.py
+++ grid-xyz-dalton.py
@@ -1,7 +1,7 @@
 # --------------------------------Import----------------------------------------#
 import csv
 
-max_bq_num = 22
+max_bq_num = 10
 atoms_num = 18
 ix = []
 iy = []
@@ -14,13 +14,13 @@ def d_range(start, stop, step):
     while r < stop:
         yield r
         r += step
-    # return R
+        # return R
 
 
 def chunks(l, n):
     """Yield successive n-sized chunks from l."""
     for i in range(0, len(l), n):
-        yield l[i:i+n]
+        yield l[i:i + n]
 
 
 def grid_gen(x_start, x_stop, y_start, y_stop, z_start, z_stop, delta):
@@ -57,7 +57,6 @@ def print_logo():
 
 # --------------------------------Main----------------------------------------#
 def main():
-
     print_logo()
     # x_start, x_stop, y_start, y_stop, z_start, z_stop, delta
     grid_gen(-3.5, 3.5, 0.0, 4.5, -3.0, 4.0, 0.5)
@@ -80,47 +79,53 @@ def main():
             cube_writer.writerow(['C9H9+'])
 
             # space line
-            cube_writer.writerow(['THIS FILE CREATED BY "BQ_ATOMS_GEN FOR DALTON"'])
+            cube_writer.writerow(['THIS FILE CREATED BY /BQ_ATOMS_GEN FOR DALTON'])
 
             # g09 key words
-            cube_writer.writerow(['Atomtypes=4 Charge=1 Angstrom Spherical'])
+            cube_writer.writerow(['Atomtypes=4 Charge=1 Generator=1 XY Angstrom Spherical'])
 
             # Geo
-            cube_writer.writerow(['Charge=6.0  Atoms=9      Basis=6-31G*'])
+            cube_writer.writerow(['Charge=6.0  Atoms=1     Basis=3-21G'])
 
             # Geo
-            cube_writer.writerow(['C', '0.','0.','1.645014194'])
-            cube_writer.writerow(['C', '0.4905022324','1.27684773','1.1554414148'])
-            cube_writer.writerow(['C', '-0.4905022324','-1.2427684773','1.1554414148'])
-            cube_writer.writerow(['C', '0.0100585215','2.0928052569','0.1040785875'])
-            cube_writer.writerow(['C', '-0.0100585215','-2.0928052569','0.1040785875'])
-            cube_writer.writerow(['C', '-0.8152087457','1.6410894083','-0.9354914584'])
-            cube_writer.writerow(['C', '0.8152087457','-1.6410894083','-0.9354914584'])
-            cube_writer.writerow(['C', '-0.6569284614','0.2832859329','-1.251946014'])
-            cube_writer.writerow(['C', '0.6569284614','-0.2832859329','-1.251946014'])
+            cube_writer.writerow(['C', '0.', '0.', '1.645014194'])
 
-            cube_writer.writerow(['Charge=1.0  Atoms=9      Basis=6-31G*'])
+            # Geo
+            cube_writer.writerow(['Charge=6.0  Atoms=8     Basis=3-21G'])
 
-            cube_writer.writerow(['H', '0.','0.','2.7459418542'])
-            cube_writer.writerow(['H', '1.0699783805','1.7858874614','1.9137457041'])
-            cube_writer.writerow(['H', '-1.0699783805','-1.7858874614','1.9137457041'])
-            cube_writer.writerow(['H', '0.12964425','3.1677255378','0.2859658405'])
-            cube_writer.writerow(['H', '-0.12964425','-3.1677255378','0.2859658405'])
-            cube_writer.writerow(['H', '-1.5936377795','2.2790433661','-1.3665504299'])
-            cube_writer.writerow(['H', '1.5936377795','-2.2790433661','-1.3665504299'])
-            cube_writer.writerow(['H', '-1.490812806','-0.3325910524','-1.6109751404'])
-            cube_writer.writerow(['H', '1.490812806','0.3325910524','-1.6109751404'])
+            cube_writer.writerow(['C', '0.4905022324', '1.27684773', '1.1554414148'])
+            cube_writer.writerow(['C', '-0.4905022324', '-1.2427684773', '1.1554414148'])
+            cube_writer.writerow(['C', '0.0100585215', '2.0928052569', '0.1040785875'])
+            cube_writer.writerow(['C', '-0.0100585215', '-2.0928052569', '0.1040785875'])
+            cube_writer.writerow(['C', '-0.8152087457', '1.6410894083', '-0.9354914584'])
+            cube_writer.writerow(['C', '0.8152087457', '-1.6410894083', '-0.9354914584'])
+            cube_writer.writerow(['C', '-0.6569284614', '0.2832859329', '-1.251946014'])
+            cube_writer.writerow(['C', '0.6569284614', '-0.2832859329', '-1.251946014'])
 
-            cube_writer.writerow(['Charge=0.0  Atoms=54      Basis=None'])
+            cube_writer.writerow(['Charge=1.0  Atoms=1     Basis=3-21G'])
+
+            cube_writer.writerow(['H', '0.', '0.', '2.7459418542'])
+
+            cube_writer.writerow(['Charge=1.0  Atoms=8     Basis=3-21G'])
+
+            cube_writer.writerow(['H', '1.0699783805', '1.7858874614', '1.9137457041'])
+            cube_writer.writerow(['H', '-1.0699783805', '-1.7858874614', '1.9137457041'])
+            cube_writer.writerow(['H', '0.12964425', '3.1677255378', '0.2859658405'])
+            cube_writer.writerow(['H', '-0.12964425', '-3.1677255378', '0.2859658405'])
+            cube_writer.writerow(['H', '-1.5936377795', '2.2790433661', '-1.3665504299'])
+            cube_writer.writerow(['H', '1.5936377795', '-2.2790433661', '-1.3665504299'])
+            cube_writer.writerow(['H', '-1.490812806', '-0.3325910524', '-1.6109751404'])
+            cube_writer.writerow(['H', '1.490812806', '0.3325910524', '-1.6109751404'])
+
+            cube_writer.writerow(['Charge=0.0  Atoms=10'])
 
             # set Bq atom coord in number of max_bq_num
             for j in range(len(chunks_by_bq[i])):
                 cube_writer.writerow(['X',
-                                      ix[max_bq_num*i+j],
-                                      iy[max_bq_num*i+j],
-                                      iz[max_bq_num*i+j]])
+                                      ix[max_bq_num * i + j],
+                                      iy[max_bq_num * i + j],
+                                      iz[max_bq_num * i + j]])
             cube_writer.writerow(['%EOF%'])
-            cube_writer.writerow([])
             csv_file.close()
             # # set weak conductivity with bq
             # for c in range(len(chunks_by_bq[i]) + atoms_num):
@@ -136,9 +141,8 @@ def main():
         cube_writer.writerow([])
         for i in range(len(chunks_by_bq)):
             cube_writer.writerow(['dalton', '-mb', '1024', '-noarch', 'test.dal', "c9h9+{}.mol".format("%06d" % i)])
-        cube_writer.writerow([])
     run_file.close()
+
 
 if __name__ == '__main__':
     main()
-
